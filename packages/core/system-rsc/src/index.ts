10f019d901e0c952ed37eb22906db8288bb687a4
xjun4882 <xjun4882@gmail.com>
2024-05-13 10:26:29 +0800

    new ui

---
+++ packages/core/system-rsc/src/index.ts
@@ -0,0 +1,30 @@
+export type {
+  As,
+  DOMElement,
+  DOMElements,
+  CapitalizedDOMElements,
+  DOMAttributes,
+  OmitCommonProps,
+  RightJoinProps,
+  MergeWithAs,
+  InternalForwardRefRenderFunction,
+  PropsOf,
+  Merge,
+  HTMLNextUIProps,
+  PropGetter,
+} from "./types";
+
+export {
+  forwardRef,
+  toIterator,
+  mapPropsVariants,
+  mapPropsVariantsWithCommon,
+  isNextUIEl,
+} from "./utils";
+
+export {extendVariants} from "./extend-variants";
+export type {
+  ExtendVariantProps,
+  ExtendVariantWithSlotsProps,
+  ExtendVariants,
+} from "./extend-variants";
